4fa74bf252cb462b18db3510b375a6624b54040c
Amit Kumar <amit.kumar12@nagarro.com>
2021-11-28 11:54:54 +0530

    load games

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,92 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React, { useState } from "react"
+
+import "../styles/styles.css"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const [isHangmanVisible, setHangmanVisible] = useState(false)
+
+  const [isTicTacToeVisible, setTicTacToeVisible] = useState(false)
+
+  const showHangManHandler = () => {
+    setTicTacToeVisible(() => {
+      return false
+    });
+
+    setIsLoading(() => {
+      return true;
+    });
+
+    setHangmanVisible(() => {
+      return true
+    });
+  }
+
+  const showTicTacToeHandler = () => {
+    setHangmanVisible(() => {
+      return false
+    });
+
+    setIsLoading(() => {
+      return true;
+    });
+
+    setTicTacToeVisible(() => {
+      return true
+    });
+  }
+
+  const resetLoader = () => {
+    setIsLoading(() => {
+      return false;
+    })
+  };
+
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <div className="container-fluid">
+        <div className="row">
+          <button
+            type="button"
+            className="btn btn-primary mr-15"
+            onClick={showHangManHandler}
+          >
+            Show Hangman
+          </button>
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={showTicTacToeHandler}
+          >
+            Show Tic Tac Toe
+          </button>
+        </div>
+        <div className="row mt-20">
+          {isLoading && <div className="alert alert-success">Game Is Loading........</div>}
+          {isHangmanVisible && (
+            <iframe
+              title="Hangman Game"
+              src="https://amit-hangman-game.netlify.app/"
+              onLoad={resetLoader}
+              className="conatiner__game"
+            ></iframe>
+          )}
+          {isTicTacToeVisible &&  <iframe
+              title="TicTacToe Game"
+              src="https://amit-tictactoe-game.netlify.app/"
+              onLoad={resetLoader}
+              className="conatiner__game"
+            ></iframe>}
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
